8f85e1811d3ededf5b929a57299ca19a1d02ab41
dariashid <dariashid@mail.ru>
2024-03-17 19:29:51 +0300

    Employee

---
+++ src/test/java/SheetsAndSets/employeeaccounting/EmployeeAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package SheetsAndSets.employeeaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
